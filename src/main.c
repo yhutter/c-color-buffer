6ca8ed063369a6a5b64f59b278b96136f5c1ba7c
yhutter <hutteryannick@gmail.com>
2024-11-30 16:17:48 +0100

    Update README and colors

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include <SDL3/SDL.h>
 
 bool running = false;
-uint32_t background_color = 0xff181818;
-uint32_t foreground_color = 0xffffdd33;
+uint32_t background_color = 0xff0f0908;
+uint32_t foreground_color = 0xffd47d49;
 int rectangle_width = 50;
 int rectangle_height = 50;
 
@@ -18,16 +18,34 @@ void check_events(void) {
 	}
 }
 
+void render_quads() {
+	int num_quads_x = 3;
+	int num_quads_y = 3;
+	int quad_size = 50;
+	int quad_offset = 50;
+	int full_width = num_quads_x * (quad_size + quad_offset);
+	int full_height = num_quads_y * (quad_size + quad_offset);
+	int x_start = (window_width - full_width) * 0.5;
+	int y_start = (window_height - full_height) * 0.5;
+	for (int i = 0; i < num_quads_y; i++) {
+		for (int j = 0; j < num_quads_x; j++) {
+			int x = (j * quad_size) + (j * quad_offset) + x_start;
+			int y = (i * quad_size) + (i * quad_offset) + y_start;
+			draw_rectangle(
+				x,
+				y,
+				quad_size,
+				quad_size,
+				foreground_color
+			);
+		}
+	}
+}
+
 void render(void) {
 	render_color_buffer();
 	clear_color_buffer(background_color);
-	draw_rectangle(
-		(window_width - rectangle_width) * 0.5,
-		(window_height - rectangle_height) * 0.5,
-		rectangle_width, 
-		rectangle_height,
-		foreground_color
-	);
+	render_quads();
 	SDL_RenderPresent(renderer);
 }
 
